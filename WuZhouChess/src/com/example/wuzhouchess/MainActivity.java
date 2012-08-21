7a32db82b28362a29028e160a2e131b3f1a04c4c
Smart Xu <smartgang@126.com>
2012-08-21 23:12:43 +0800

    1、完成游戏控制功能，开始，暂停，重新开始 2、代码结构重新规划 3、学习使用Handler和Thread 遗留问题： Thread的停止和重启问题，暂时没有较好的办法

--- WuZhouChess/src/com/example/wuzhouchess/MainActivity.java
+++ WuZhouChess/src/com/example/wuzhouchess/MainActivity.java
@@ -2,23 +2,46 @@ package com.example.wuzhouchess;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.util.DisplayMetrics;
+import android.content.Intent;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-	ChessView cb;
+//	ChessView cb;
+	Button topBtn;
+	Button bottomBtn;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cb=(ChessView)findViewById(R.id.chess);
-        DisplayMetrics dm=new DisplayMetrics();  
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        cb.setChessBoard(dm);
-        cb.invalidate();
+        topBtn=(Button)findViewById(R.id.button1);
+        bottomBtn=(Button)findViewById(R.id.button2);
+        topBtn.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent=new Intent(MainActivity.this,PlayActivity.class);
+				intent.putExtra("PlayerType", "H2H");
+				startActivity(intent); 
+			}
+        	
+        });
+        bottomBtn.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent=new Intent(MainActivity.this,PlayActivity.class);
+				intent.putExtra("PlayerType", "H2AI");
+				startActivity(intent); 
+			}
+        	
+        });
     }
 
     @Override
--- WuZhouChess/src/com/example/wuzhouchess/Views/ChessView.java
+++ WuZhouChess/src/com/example/wuzhouchess/Views/ChessView.java
@@ -1,20 +1,29 @@
 /**
  * 
  */
-package com.example.wuzhouchess;
+package com.example.wuzhouchess.Views;
 
 import java.util.ArrayList;
 
+import com.example.classes.Chess;
+import com.example.classes.ChessBoard;
+import com.example.classes.Movement;
+import com.example.classes.Player;
+import com.example.classes.TimeCounter;
+import com.example.wuzhouchess.PlayActivity;
+
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
-import android.util.DisplayMetrics;
 import android.view.MotionEvent;
-import android.view.View.OnClickListener;
 import android.widget.Toast;
-
+import android.util.Log;
 
 
 /**五洲棋
@@ -22,41 +31,67 @@ import android.widget.Toast;
  * @version 1.0
  */
 public class ChessView extends android.view.View {
-
+	
+	final private static String LOG_TGA="ChessView";
+	
+	//定义游戏的各个状态
+	final public static int GameStatus_Ready=1;
+	final public static int GameStatus_Playing=2;
+	final public static int GameStatus_Pause=3;
+	final public static int GameStatus_Over=4;
+	public int gameStatus=GameStatus_Ready;
+	
 	//定义棋子的两个颜色
 	final public static int ChessColor_Black=1;
 	final public static int ChessColor_White=2;
+	final public static int ChessColor_Yellow=3;
 	final public static int GridEmpty=0;
-	//用来表示轮到哪一下颜色的选手走棋
-	public int playerTurn;
+
+	//定义onTouch与actionHandler消息传递的值，用于与用户操作交互
+//	final public static int ACTION_MOVE=1;
+	
 	//用于表示当前的按键状态，非空则表示当前已经选中棋子
 	Chess choseChess=null;
+	Chess movedChess=null;
 	//定义棋盘上每一格的宽高,默认为20，实际根据屏幕大小来计算
 	private int boardGridLength=20;
 	//棋盘左上角的坐标，根据屏幕中心点的位置，结合每一格的宽高来计算，默认是600*480
 	private int boardX=80;
 	private int boardY=140;
 	//两个颜色的棋子序列，初始各为5个，减为0个则输，达到10个判赢
-	private ArrayList<Chess> blackChessArray= new ArrayList<Chess>();
-	private ArrayList<Chess> whiteChessArray= new ArrayList<Chess>();
+	private ArrayList<Chess> blackChessArray;
+	private ArrayList<Chess> whiteChessArray;
 	ChessBoard cb;
+	Player playerWhite;
+	Player playerBlack;
+	Player currentPlayer;
+	private TimeCounter timeCounter;
+	private Handler timeCounterHandler;
+	private Handler viewHolderHandler;
+	private Handler actionHandler;
+	private Thread actionThread;
 	
+	private boolean isTouched;
+	/**
+	 * @param viewHolerHandler the viewHolerHandler to set
+	 */
+	public void setViewHolderHandler(Handler viewHolderHandler) {
+		this.viewHolderHandler = viewHolderHandler;
+	}
+
+	private boolean isWaiting=true;
+	private int moveTargetX;
+	private int moveTargetY;
+	
+	private int currentRoundTime;
 	
 	public ChessView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
-		//初始始化棋盘及两列棋子，黑棋在上，白棋在下
-		for(int i=0;i<5;i++)
-		{
-			Chess chess=new Chess(i,0,ChessColor_Black);
-			blackChessArray.add(chess);
-			Chess chess2=new Chess(i,4,ChessColor_White);
-			whiteChessArray.add(chess2);
-			cb=new ChessBoard();
-			//黑方先走
-			playerTurn=ChessColor_Black;
-			
-		}
+		
+		initialCounterAndHandlers();
+		initialChessBorad();
+
 	}
 
 	/**
@@ -67,17 +102,8 @@ public class ChessView extends android.view.View {
 	public ChessView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		// TODO Auto-generated constructor stub
-		//初始始化棋盘及两列棋子，黑棋在上，白棋在下
-		for(int i=0;i<5;i++)
-		{
-			Chess chess=new Chess(i,0,ChessColor_Black);
-			blackChessArray.add(chess);
-			Chess chess2=new Chess(i,4,ChessColor_White);
-			whiteChessArray.add(chess2);
-			cb=new ChessBoard();
-			//黑方先走
-			playerTurn=ChessColor_Black;
-		}
+		initialCounterAndHandlers();
+		initialChessBorad();
 	}
 
 	/**
@@ -86,7 +112,90 @@ public class ChessView extends android.view.View {
 	public ChessView(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
+		initialCounterAndHandlers();
+		initialChessBorad();
+	}	
+	//初始化计算器和handler
+	private void initialCounterAndHandlers()
+	{
+		timeCounterHandler=new Handler(){
+
+			/* (non-Javadoc)
+			 * @see android.os.Handler#handleMessage(android.os.Message)
+			 */
+			@Override
+			public void handleMessage(Message msg) {
+				// TODO Auto-generated method stub
+				currentRoundTime=msg.what;
+//				Log.d(LOG_TGA,"updating currentRoundTime");
+				//更新计时器后刷新整个显示
+				updateView();
+			}
+			
+		};
+		//创建timeCounter并添加handler
+		timeCounter=new TimeCounter(timeCounterHandler);
+		
+		actionHandler=new Handler(){
+
+			/* (non-Javadoc)
+			 * @see android.os.Handler#handleMessage(android.os.Message)
+			 */
+			@Override
+			public void handleMessage(Message msg) {
+				// TODO Auto-generated method stub
+				invalidate();
+			}
+			
+		};
+		
+		actionThread=new Thread(new Runnable()
+		{
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+	            try {
+					while(gameStatus!=GameStatus_Pause&&gameStatus!=GameStatus_Over)
+					{	
+						//等待用户操作,0.2秒检测一次
+						while(isWaiting&&movedChess==null)Thread.sleep(200);
+						Log.d(LOG_TGA,"user ating");
+						//由用户进行移动判断，当用户是人类玩家时，其实在OnTouch中已经做过isMoveable的判断
+						//AI时没做判断,增加AI判断的话，需要考虑如何将判断结果反馈回给AI？
+						Movement movement=currentPlayer.move(cb, movedChess, moveTargetX, moveTargetY);
+						//之后移动棋盘上的内容
+						move(movedChess,movement.toX,movement.toY);
+						eat(movedChess,moveTargetX,moveTargetY);
+						int win=isWin();
+						if(win!=0)
+						{
+							//游戏结束
+							gameOver();
+							Message msg=new Message();
+							msg.obj=gameStatus;
+							msg.what=GameStatus_Over;
+							viewHolderHandler.sendMessage(msg);
+						}
+						movedChess=null;
+						//交换用户
+						switchPlayer();
+						//交换用户后如果当前用户是人类玩家，则需要等待用户操作
+						if(currentPlayer.getPlayerType()==Player.PlayerType_Human)isWaiting=true;
+						updateView();
+					}
+	            } catch (InterruptedException e) {
+	                e.printStackTrace();
+	            }
+			}
+		});		
+	}
+	//初始化棋盘，将棋子恢复到原位
+	private void initialChessBorad()
+	{
 		//初始始化棋盘及两列棋子，黑棋在上，白棋在下
+		blackChessArray=new ArrayList<Chess>();
+		whiteChessArray=new ArrayList<Chess>();
 		for(int i=0;i<5;i++)
 		{
 			Chess chess=new Chess(i,0,ChessColor_Black);
@@ -94,10 +203,39 @@ public class ChessView extends android.view.View {
 			Chess chess2=new Chess(i,4,ChessColor_White);
 			whiteChessArray.add(chess2);
 			cb=new ChessBoard();
-			//黑方先走
-			playerTurn=ChessColor_Black;
 		}
-	}	
+	}
+	
+	private void updateView()
+	{
+		Message msg;
+		actionHandler.sendEmptyMessage(0);
+	}
+	//走完一步后，轮到下一方
+	private void switchPlayer()
+	{
+		Log.d(LOG_TGA,"switchPlayer");
+		//加上totalPlayTime
+		currentPlayer.setTotalPlayTime(currentRoundTime+currentPlayer.getTotalPlayTime());
+		if(currentPlayer.getColor()==Player.ChessColor_Black)currentPlayer=playerWhite;
+		else currentPlayer=playerBlack;
+		//重置计数器
+		timeCounter.resetCounter();
+		currentRoundTime=0;
+	}
+	
+	/**设置玩家
+	 * @param white
+	 * @param black
+	 */
+	public void setPlayer(Player white,Player black)
+	{
+		playerWhite=white;
+		playerBlack=black;
+		playerBlack.setChessArray(blackChessArray);
+		playerWhite.setChessArray(whiteChessArray);
+		currentPlayer=playerBlack;
+	}
 	
 	/* (non-Javadoc)
 	 * @see android.view.View#onTouchEvent(android.view.MotionEvent)
@@ -109,66 +247,52 @@ public class ChessView extends android.view.View {
 		//up和down均会触发这个函数
 //		if(event.getAction()==MotionEvent.ACTION_UP)
 //		{
+		isTouched=!isTouched;
+		if(isTouched==false)return true;
+		Log.d(LOG_TGA,"onTouchEvent");
+		if(currentPlayer.getPlayerType()==Player.PlayerType_AI)return true;
 			Chess c=getTouchItem((int)event.getX(),(int)event.getY());
 			//没有选中位置，则直接返回
 			if(c==null||c.x>4||c.y>4)return true;
 			//choseChess为null,则当前没有选中棋子
 			if(null==choseChess)
 			{
-				if(playerTurn==ChessColor_Black)
+				Log.d(LOG_TGA,"onTouchEvent selecting");
+				for(int i=0;i<currentPlayer.getChessArray().size();i++)
 				{
-					for(int i=0;i<blackChessArray.size();i++)
+					if((c.x==currentPlayer.getChessArray().get(i).x)&&c.y==currentPlayer.getChessArray().get(i).y)
 					{
-						if((c.x==blackChessArray.get(i).x)&&c.y==blackChessArray.get(i).y)
-						{
-							choseChess=c;
-							choseChess.color=blackChessArray.get(i).color;
-							invalidate();
-							return true;
-						}
-					}
-				}
-				else 
-				{
-					for(int i=0;i<whiteChessArray.size();i++)
-					{
-						if((c.x==whiteChessArray.get(i).x)&&c.y==whiteChessArray.get(i).y)
-						{
-							choseChess=c;
-							choseChess.color=whiteChessArray.get(i).color;
-							invalidate();
-							return true;
-						}
+						choseChess=c;
+						choseChess.color=currentPlayer.getChessArray().get(i).color;
+						updateView();
+						return true;
 					}
 				}
 			}
-			else if(choseChess.color==playerTurn)
+			else if(choseChess.color==currentPlayer.getColor())
 			{	//如果当前已经有选中，第二次按下时移动成功，则判断为移动
-				if(true==move(choseChess, c.x,c.y))
+				if(true==isMovable(choseChess, c.x,c.y))
 				{
-					eat(choseChess,c.x,c.y);
+					Log.d(LOG_TGA,"onTouchEvent moving");
+					movedChess=new Chess(choseChess.x,choseChess.y,choseChess.color);
 					choseChess=null;
-					if(playerTurn==ChessColor_Black)playerTurn=ChessColor_White;
-					else playerTurn=ChessColor_Black;
-					invalidate();
-					int win=isWin();
-					if(win!=0)
-					{
-						if(win==ChessColor_White)Toast.makeText(this.getContext(), "白方获胜", Toast.LENGTH_SHORT).show();
-						else Toast.makeText(this.getContext(), "黑方获胜", Toast.LENGTH_SHORT).show();
-					}
+					//通过action线程，用户已完成移动
+					moveTargetX=c.x;
+					moveTargetY=c.y;
+					isWaiting=false;
 				}
 				else if(cb.chessBoard[c.x][c.y]==choseChess.color)//第二次按下时移动失败，则要判断是否选中了其他的棋子
 				{
 					choseChess.x=c.x;
 					choseChess.y=c.y;
-					invalidate();
+					updateView();
 				}
 			}
 			return true;
 //		}
 //		return false;
 	}
+	
 	
 	private Chess getTouchItem(int coordinateX,int coordinateY)
 	{
@@ -194,96 +318,127 @@ public class ChessView extends android.view.View {
 	@Override
 	protected void onDraw(Canvas canvas) {
 		// TODO Auto-generated method stub
-		super.onDraw(canvas);
-		cb.draw(canvas);
-		for(int i=0;i<blackChessArray.size();i++)blackChessArray.get(i).draw(canvas);
-		for(int i=0;i<whiteChessArray.size();i++)whiteChessArray.get(i).draw(canvas);
+		super.onDraw(canvas);	
+		if(gameStatus==GameStatus_Playing)
+		{	
+			Log.d(LOG_TGA,"onDrawing");
+			cb.draw(canvas,boardX, boardY,boardGridLength);
+			for(int i=0;i<blackChessArray.size();i++)blackChessArray.get(i).draw(canvas, boardX, boardY, boardGridLength,ChessColor_Black);
+			for(int i=0;i<whiteChessArray.size();i++)whiteChessArray.get(i).draw(canvas,boardX, boardY, boardGridLength,ChessColor_White);
+			if(null!=choseChess)choseChess.draw(canvas, boardX, boardY, boardGridLength,  ChessColor_Yellow);	
+			if(choseChess==null)Log.d(LOG_TGA,"choseChess is null");
+			drawTotalTime(canvas);
+			drawCurrentRoundTime(canvas);
+		}
 	}
 
-	public void setChessBoard(DisplayMetrics metrics)
+	//左棋盘上下方的左侧，分别显示两边选择总共花费的时间,黑上白下
+	private void drawTotalTime(Canvas canvas)
 	{
-		 int heigth=metrics.heightPixels;
-		 int width=metrics.widthPixels;
-		 
-		 //横向划分为6格，棋盘占4格，左右各空两格,竖向的放中间
-		 boardGridLength=width/6;
-		 boardX=boardGridLength+1;
-		 boardY=heigth/2-boardGridLength*2;
+		int blackTime=playerBlack.getTotalPlayTime();
+		int whiteTime=playerWhite.getTotalPlayTime();
+		Paint paint=new Paint();
+		paint.setTextSize(boardGridLength/2);//字体在小设为间隔的一半
+		paint.setColor(Color.LTGRAY);
+		String time=String.valueOf(blackTime/3600)+":"
+						+String.valueOf((blackTime%3600)/60)+":"
+						+String.valueOf(blackTime%60);
+		//上下要对称，所以这里boardGridLength除以2是必要的
+		canvas.drawText(time, boardX, boardY-boardGridLength/2, paint);
+		String time2=String.valueOf(whiteTime/3600)+":"
+						+String.valueOf((blackTime%3600)/60)+":"
+						+String.valueOf(whiteTime%60);
+		canvas.drawText(time2, boardX, boardY+boardGridLength*5, paint);
 	}
-
-	//将棋子从自身位置移动到指定坐标，返回移动结果
-	public boolean move(Chess chess, int x,int y)
+	//画出当前轮用户游戏时间
+	private void drawCurrentRoundTime(Canvas canvas)
+	{
+		//位置起始点，根据用户颜色的不同，黑上白下
+		int startY;
+		if(currentPlayer.getColor()==Player.ChessColor_Black)startY=boardY-boardGridLength/2;
+		else startY=boardY+boardGridLength*5;
+		Paint paint=new Paint();
+		paint.setTextSize(boardGridLength/2);//字体在小设为间隔的一半
+		paint.setColor(Color.LTGRAY);
+		String time=String.valueOf(currentRoundTime/3600)+":"
+						+String.valueOf((currentRoundTime%3600)/60)+":"
+						+String.valueOf(currentRoundTime%60);
+		canvas.drawText(time, boardX+boardGridLength*2, startY, paint);
+	}
+	public void setChessBoard()
+	{
+		 //横向划分为6格，棋盘占4格，左右各空两格,竖向的放中间
+		int left=getLeft();
+		int top=getTop();
+		int right=getRight();
+		int bottom=getBottom();		
+		boardGridLength=(right-left)/6;
+		boardX=boardGridLength+1;
+		boardY=(bottom-top)/2-boardGridLength*2;
+	}
+	//开始
+	public void gameStart()
+	{
+		Log.d(LOG_TGA,"gameStart");
+		isWaiting=true;
+		timeCounter.startCounter();
+		timeCounter.start();
+		actionThread.start();
+		gameStatus=GameStatus_Playing;
+	}
+	//游戏结束后重新开始
+	public void gameRestart()
+	{
+		isWaiting=true;
+		timeCounter.startCounter();	
+		gameStatus=GameStatus_Playing;
+		initialChessBorad();
+	}
+	//暂停
+	public void gamePause()
+	{
+		timeCounter.pauseCounter();
+		gameStatus=GameStatus_Pause;
+	}
+	//暂停后恢复
+	public void gameResume()
+	{
+		timeCounter.startCounter();
+		gameStatus=GameStatus_Playing;
+	}
+	public void gameOver()
+	{
+		Log.d(LOG_TGA,"gameOver");
+		isWaiting=false;
+		timeCounter.stopCounter();
+//		actionThread.stop();
+		gameStatus=GameStatus_Over;
+	}
+	//判断将chess移动到x,y是否可行
+	public boolean isMovable(Chess chess, int x, int y)
 	{
 		if(cb.isOccupied(x,y)==false)return false;//如果目标格已被占用，直接返回失败
 		//判断是否是向上下左右移动
 		if(((1==Math.abs(x-chess.x))&&(0==Math.abs(y-chess.y)))||//左右移动
 				((0==Math.abs(x-chess.x))&&(1==Math.abs(y-chess.y))))//上下移动
 		{
-			cb.chessBoard[chess.x][chess.y]=GridEmpty;
-			cb.chessBoard[x][y]=chess.color;
-			if(chess.color==ChessColor_Black)
-			{
-				for(int i=0;i<blackChessArray.size();i++)
-				{
-					Chess c=blackChessArray.get(i);
-					if((chess.x==c.x)&&chess.y==c.y)
-					{
-						c.x=x;
-						c.y=y;
-						return true;
-					}
-				}
-			}
-			else
-			{
-				for(int i=0;i<whiteChessArray.size();i++)
-				{
-					Chess c=whiteChessArray.get(i);
-					if((chess.x==c.x)&&chess.y==c.y)
-					{
-						c.x=x;
-						c.y=y;
-						return true;
-					}
-				}
-			}
+			return true;
 		}
 		else if((1==Math.abs(x-chess.x))&&(1==Math.abs(y-chess.y)))//斜线移动
 		{
 			if((chess.x+chess.y)%2==0)//从棋盘上看，点左右各为偶数的占可斜线移动
 			{
-				cb.chessBoard[chess.x][chess.y]=GridEmpty;
-				cb.chessBoard[x][y]=chess.color;
-				if(chess.color==ChessColor_Black)
-				{
-					for(int i=0;i<blackChessArray.size();i++)
-					{
-						Chess c=blackChessArray.get(i);
-						if((chess.x==c.x)&&chess.y==c.y)
-						{
-							c.x=x;
-							c.y=y;
-							return true;
-						}
-					}
-				}
-				else
-				{
-					for(int i=0;i<whiteChessArray.size();i++)
-					{
-						Chess c=whiteChessArray.get(i);
-						if((chess.x==c.x)&&chess.y==c.y)
-						{
-							c.x=x;
-							c.y=y;
-							return true;
-						}
-					}
-				}
+				return true;
 			}
-			else return false;
 		}
-		return false;
+		Log.d(LOG_TGA,"moving is true");
+		return false;		
+	}
+	//将棋子从自身位置移动到指定坐标，返回移动结果
+	public void move(Chess chess, int x,int y)
+	{
+		cb.chessBoard[chess.x][chess.y]=GridEmpty;
+		cb.chessBoard[x][y]=chess.color;
 	}
 	//将chess移动到目标x,y后，要判断是否吃子，并将被吃的子做为结果返回
 	//函数前提是前面已经做过move的判断，并且为成功，所以这里不对move进行判断
@@ -320,16 +475,11 @@ public class ChessView extends android.view.View {
 	//在一条模，纵，斜线上，移动后该子与相邻同色子顶着另一色子，并且该线上没有其他的棋子
 	public int eatDing(ArrayList<Chess> eatResult,Chess chess, int x, int y)
 	{
-//		ArrayList<Chess> eatResult= new ArrayList<Chess>();
 		if(eatResult==null)return 0;
 		int eatCount=0;
-//		ArrayList<Chess> myArray=blackChessArray;
-//		ArrayList<Chess> yourArray=whiteChessArray;
 		int yourColor=ChessColor_White;
 		if(chess.color==ChessColor_White)
 		{
-//			myArray=whiteChessArray;
-//			yourArray=blackChessArray;
 			yourColor=ChessColor_Black;
 		}
 		//分四步进行判断：横，竖，左斜，右斜
@@ -758,78 +908,4 @@ public class ChessView extends android.view.View {
 		return 0;
 	}
 	
-	//定义一个5*5的棋盘，总共10个棋子，每一个的内容为棋子的颜色
-	private class ChessBoard{
-		private int[][] chessBoard;
-		//初始化棋盘
-		public ChessBoard() {
-			chessBoard=new int[5][5];
-			for(int i=0;i<5;i++)
-			{
-				chessBoard[i][0]=ChessColor_Black;
-				chessBoard[i][4]=ChessColor_White;
-			}
-		}
-		
-		//在画布上画出自己
-		private void draw(Canvas canvas)
-		{
-			Paint p=new Paint();
-			p.setColor(Color.BLUE);
-			p.setStrokeWidth(2);
-			int boardGridLength_2=boardGridLength*2;
-			int boardGridLength_4=boardGridLength*4;
-			//画横线和竖线
-			for(int i=0;i<5;i++)
-			{
-				canvas.drawLine(boardX, boardY+boardGridLength*i, boardX+boardGridLength_4, boardY+boardGridLength*i, p);
-				canvas.drawLine(boardX+boardGridLength*i, boardY, boardX+boardGridLength*i, boardY+boardGridLength_4, p);
-			}
-			//三条左斜线
-			canvas.drawLine(boardX, boardY, boardX+boardGridLength_4, boardY+boardGridLength_4, p);
-			canvas.drawLine(boardX, boardY+boardGridLength_2, boardX+boardGridLength_2, boardY+boardGridLength_4, p);
-			canvas.drawLine(boardX+boardGridLength_2, boardY, boardX+boardGridLength_4, boardY+boardGridLength_2, p);
-			//三条右斜线
-			canvas.drawLine(boardX+boardGridLength_4, boardY, boardX, boardY+boardGridLength_4, p);
-			canvas.drawLine(boardX+boardGridLength_2, boardY, boardX, boardY+boardGridLength_2, p);
-			canvas.drawLine(boardX+boardGridLength_4, boardY+boardGridLength_2, boardX+boardGridLength_2, boardY+boardGridLength_4, p);
-		}
-
-		//判断当前位置是否已经有棋子占用
-		private boolean isOccupied(int x, int y)
-		{
-			if(x<0||x>4)return false;
-			if(y<0||y>4)return false;
-			if(chessBoard[x][y]==GridEmpty)return true;
-			return false;
-		}
-		
-	}
-	//棋子类
-	private class Chess {
-		public int x;
-		public int y;
-		public int color;
-		/**
-		 * @param x
-		 * @param y
-		 * @param color
-		 */
-		public Chess(int x, int y, int color) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.color = color;
-		}
-		//在画布上画棋子，以x,y为中心坐标，根据颜色的不同画一个圆，半径为棋格大小的1/4
-		private void draw(Canvas canvas)
-		{
-			Paint p=new Paint();
-			//判断自身是否就是选中的棋子，是的话画成黄色
-			if(choseChess!=null&&this.x==choseChess.x&&this.y==choseChess.y)p.setColor(Color.YELLOW);
-			else if(ChessColor_Black==color)p.setColor(Color.BLACK);
-			else p.setColor(Color.LTGRAY);
-			canvas.drawCircle(boardX+x*boardGridLength, boardY+y*boardGridLength, boardGridLength/4, p);
-		}
-	}
 }
